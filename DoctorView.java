07179549654ed610d20d1c994ff306f8df773479
SpongeBob341 <divyansh201103@gmail.com>
2024-04-09 17:27:28 -0700

    gavin file copied

--- DoctorView.java
+++ DoctorView.java
@@ -33,64 +33,74 @@ public class DoctorView{
     
 
     private void initializeUI()
+{
+    //Label
+    docNote = new Label("Doctor Notes:");
+    meds = new Label("Prescription:");
+
+    //Text Area
+    docNoteText = new TextArea();
+    medstext = new TextArea();
+
+    docNoteText.setPrefWidth(400);
+    docNoteText.setPrefHeight(50);
+    medstext.setPrefWidth(400);
+    medstext.setPrefHeight(50);
+
+    //Button
+    save = new Button("Save");
+    save.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
+    save.setPrefWidth(100);
+    save.setPrefHeight(50);
+    save.setOnAction(e -> doctorNotes(docNoteText.getText()));
+
+    sendMeds = new Button("send meds");
+    sendMeds.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
+    sendMeds.setPrefWidth(100);
+    sendMeds.setPrefHeight(50);
+    sendMeds.setOnAction(e -> doctorMedication(medstext.getText()));
+
+    viewHistory = new Button("View his");
+    viewHistory.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
+    viewHistory.setPrefWidth(100);
+    viewHistory.setPrefHeight(50);
+    if (!isHistory) 
     {
-        //Label
-        docNote = new Label("Doctor Notes:");
-        meds = new Label("Prescription:");
-
-        //Text Area
-        docNoteText = new TextArea();
-        medstext = new TextArea();
-
-        docNoteText.setPrefWidth(400);
-        docNoteText.setPrefHeight(50);
-        medstext.setPrefWidth(400);
-        medstext.setPrefHeight(50);
-
-        //Button
-        save = new Button("Save");
-        save.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
-        save.setPrefWidth(100);
-        save.setPrefHeight(50);
-        save.setOnAction(e -> doctorNotes(docNoteText.getText()));
-
-        sendMeds = new Button("send meds");
-        sendMeds.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
-        sendMeds.setPrefWidth(100);
-        sendMeds.setPrefHeight(50);
-        sendMeds.setOnAction(e -> doctorMedication(medstext.getText()));
-
-        viewHistory = new Button("View his");
-        viewHistory.setStyle("-fx-background-color: #4c6fb5; -fx-text-fill: #111112;");
-        viewHistory.setPrefWidth(100);
-        viewHistory.setPrefHeight(50);
-    
-        viewHistory.setOnAction(e -> mainApp.openHistory(isHistory));
-        
-
-        // Layout
-        mainLayout = new BorderPane();
-
-        VBox doctorNotesVBox = new VBox(docNote, docNoteText);
-        doctorNotesVBox.setSpacing(10);
-
-        VBox medicationsVBox = new VBox(meds, medstext);
-        medicationsVBox.setSpacing(10);
-
-        mainLayout.setCenter(new VBox(doctorNotesVBox, medicationsVBox));
-        mainLayout.setPadding(new Insets(20)); // Add padding around the VBox
-
-        HBox bottomBox = new HBox();
-        bottomBox.setSpacing(10);
-        bottomBox.setPadding(new Insets(10, 0, 0, 0)); // Add padding to the bottom
-
-        Button goBackButton = new Button("Go Back");
-        goBackButton.setOnAction(e -> mainApp.showMainMenu());
-        bottomBox.getChildren().addAll(goBackButton, save, sendMeds, viewHistory);
-
-        mainLayout.setBottom(bottomBox);
-
+        viewHistory.setOnAction(e -> openHistory());
     }
+    else
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText("No history found");
+        alert.showAndWait();
+    }
+    
+
+    // Layout
+    mainLayout = new BorderPane();
+
+    VBox doctorNotesVBox = new VBox(docNote, docNoteText);
+    doctorNotesVBox.setSpacing(10);
+
+    VBox medicationsVBox = new VBox(meds, medstext);
+    medicationsVBox.setSpacing(10);
+
+    mainLayout.setCenter(new VBox(doctorNotesVBox, medicationsVBox));
+    mainLayout.setPadding(new Insets(20)); // Add padding around the VBox
+
+    HBox bottomBox = new HBox();
+    bottomBox.setSpacing(10);
+    bottomBox.setPadding(new Insets(10, 0, 0, 0)); // Add padding to the bottom
+
+    Button goBackButton = new Button("Go Back");
+    goBackButton.setOnAction(e -> mainApp.showMainMenu());
+    bottomBox.getChildren().addAll(goBackButton, save, sendMeds, viewHistory);
+
+    mainLayout.setBottom(bottomBox);
+
+}
  
     //get root for the main button to use
     public BorderPane getRoot() {
@@ -125,6 +135,21 @@ public class DoctorView{
         } catch (IOException e) {
             e.printStackTrace();
         }
+	}
+
+    private void openHistory() 
+    {
+        String filename = ""; // will decide later 
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line); // Print the for now
+            }
+        } catch (IOException e) {
+            // Handle exceptions here
+            e.printStackTrace();
+        }
+
 	}
     
 }
